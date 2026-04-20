10ac36fe2e329d7b9a9190d0d160f6000bbf78d1
brooklynprisbrey-duckie <brooklynprisbrey@mail.weber.edu>
2026-04-19 23:44:19 -0600

    We're at a working version! I have 15 minutes to zip this, no time for beg test.

--- src/main.cpp
+++ src/main.cpp
@@ -63,25 +63,69 @@ int main() {
 
         bool studyReports = true;
         while (studyReports) {
-            cout << "Do you want to 0. Quit 1. examine reports 2. compare?" << endl;
+            cout << "Do you want to 0. Make more reports, 1. Examine reports, 2. Compare?" << endl;
             int choice1 = reportFolder[0].validate(0, 2);
             int reportNum = 0;
             do {
                 cout << "Which report?" << endl;
-                reportNum = reportFolder[0].validate(1, reportFolder.size());
-                cout << "Is the report for year " << reportFolder[reportNum].year <<
+                reportNum = reportFolder[0].validate(1, reportFolder.size())-1;
+                cout << "Is the report for " << reportFolder[reportNum].getDate() << " correct? 0. No 1. Yes" << endl;
+                reportNum = reportFolder[reportNum].validate(0,1);
             } while (!reportNum);
+
+            string catEnum = "0. Day, 1. Crops Sold, 2. Crop Revenue, 3. Livestock Revenue, 4. Expenses, 5. Total Revenue, 6. Net Profit";
             switch (choice1) {
             case 1: {
                 cout << "1. Look at specific day, 2. Search days, 3. Pull days from report" << endl;
-                int choice2 = reportFolder[0].validate(1, 3);
-                switch (choice2) {
-                case 1: 
+                int choice2 = reportFolder[reportNum].validate(1, 3);
+                if (choice2 == 1) {
+                    cout << "Pick the day, 1-28" << endl;
+                    int day = reportFolder[reportNum].validate(1, 28) - 1;
+                    cout << "Which category? " << catEnum << endl;
+                    DataCode goGet = static_cast<DataCode>(reportFolder[reportNum].validate(0, 6));
+                    cout << "There was " << reportFolder[reportNum].getter(day, goGet) << reportFolder[reportNum].enumString(goGet) << "on " << day << ", " << reportFolder[reportNum].getDate() << endl;
+                    continue;
+                }
+                else if (choice2 == 2) {
+                    cout << "1. Maximum, 2. Minimum, or 3. Sum?" << endl;
+                    int choice3 = reportFolder[reportNum].validate(1, 3);
+                    cout << "Which category? " << catEnum << endl;
+                    DataCode goGet = static_cast<DataCode>(reportFolder[reportNum].validate(0, 6));
+                    switch (choice3) {
+                    case 1:
+                        reportFolder[reportNum].getMax(goGet);
+                        continue;
+                    case 2:
+                        reportFolder[reportNum].getMin(goGet);
+                        continue;
+                    case 3:
+                        reportFolder[reportNum].getSum(goGet);
+                        continue;
+                    }
+                    continue;
+                }
+                else {
+                    reportFolder[reportNum].getFoundDays();
+                    continue;
                 }
             }
+            case 2: {
+                int reportNumC = 0;
+                do {
+                    cout << "Compare with which report?" << endl;
+                    reportNumC = reportFolder[0].validate(1, reportFolder.size())-1;
+                    cout << "Is the report for " << reportFolder[reportNumC].getDate() << " correct? 0. No 1. Yes" << endl;
+                    reportNumC = reportFolder[reportNumC].validate(0, 1);
+                } while (!reportNum);
+                reportFolder[reportNum].reportComparison(&reportFolder[reportNumC]);
+                continue;
+            }
+            default:
+                studyReports = false;
             }
         }
-
+        cout << "Are you done? 0. No 1. Yes" << endl;
+        reporting = !(reportFolder[0].validate(0, 1));
     } while (reporting);
 
     delete[] quarters;
